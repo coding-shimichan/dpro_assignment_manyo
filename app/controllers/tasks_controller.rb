08abd1db2cfe60dcc9f0fec8c2a3ab71f9ca0d11
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-29 19:37:55 +0900

    Feat: Pagination for Task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
 
   # GET /tasks or /tasks.json
   def index
-    @tasks = Task.order(created_at: :desc)
+    @tasks = Task.order(created_at: :desc).page(params[:page])
   end
 
   # GET /tasks/1 or /tasks/1.json
