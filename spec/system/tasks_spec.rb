df916302be7a198d77975f96ec5feaec6c1e7b7f
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-29 18:08:57 +0900

    Test: Display tasks in descending order of created_at

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -365,12 +365,31 @@ RSpec.describe 'タスク管理機能', type: :system do
 
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
-      it '登録済みのタスク一覧が表示される' do
-        FactoryBot.create(:task)
-        
+      let!(:first_task) { FactoryBot.create(:task, title: 'first_task', created_at: Time.zone.now) }
+      let!(:second_task) { FactoryBot.create(:task, title: 'second_task', created_at: 1.day.from_now) }
+      let!(:third_task) { FactoryBot.create(:task, title: 'third_task', created_at: 1.day.ago) }
+
+      before do
         visit tasks_path
-        expect(page).to have_content I18n.t("tasks.index.title")
-        expect(page).to have_content "書類作成"
+      end
+
+      it '作成済みのタスク一覧が作成日時の降順で表示される' do
+        within "#tasks-table" do
+          task_titles = all(".task-title").map(&:text)
+          expect(task_titles).to eq %w(second_task first_task third_task)
+        end
+      end
+
+      context '新たにタスクを作成した場合' do
+        it '新しいタスクが一番上に表示される' do
+          new_task = FactoryBot.create(:task, title: "forth_task", created_at: 2.day.from_now)
+          
+          visit tasks_path
+          within "#tasks-table" do
+            task_titles = all(".task-title").map(&:text)
+            expect(task_titles).to eq %w(forth_task second_task first_task third_task)
+          end
+        end
       end
     end
   end
