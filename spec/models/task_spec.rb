f443e1502b5db8d4bd9372332ecd80e6e3134260
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-30 11:15:12 +0900

    Fix: Test for Task - prepare for adding attributes

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe Task, type: :model do
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空文字の場合' do
       it 'バリデーションに失敗する' do
-        task = Task.create(title:"", content:"Buy a milk")
+        task = FactoryBot.build(:task, title: nil)
         expect(task).not_to be_valid
       end
     end
 
     context 'タスクの説明が空文字の場合' do
       it 'バリデーションに失敗する' do
-        task = Task.create(title:"Buy a milk", content:"")
+        task = FactoryBot.build(:task, content: nil)
         expect(task).not_to be_valid
       end
     end
 
-    context 'タスクのタイトルと説明に値が入っている場合' do
+    context 'タスクの属性全てに値が入っている場合' do
       it 'タスクを登録できる' do
-        task = Task.create(title:"Buy a milk", content:"Buy a milk at supermarket")
+        task = FactoryBot.build(:task)
         expect(task).to be_valid
       end
     end
