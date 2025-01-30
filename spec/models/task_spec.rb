20c2f365a81df7b55622bb744f88cbb8c523b06d
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-30 11:20:36 +0900

    Test: Validation test for added attributes of Task
    Add validation tests for the added attributes of Task - deadline_on, priority, status

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -16,6 +16,27 @@ RSpec.describe Task, type: :model do
       end
     end
 
+    context 'タスクの終了期限が空の場合' do
+      it 'バリデーションに失敗する' do
+        task = FactoryBot.build(:task, deadline_on: nil)
+        expect(task).not_to be_valid
+      end
+    end
+
+    context 'タスクの優先度が空の場合' do
+      it 'バリデーションに失敗する' do
+        task = FactoryBot.build(:task, priority: nil)
+        expect(task).not_to be_valid
+      end
+    end
+
+    context 'タスクのステータスが空の場合' do
+      it 'バリデーションに失敗する' do
+        task = FactoryBot.build(:task, status: nil)
+        expect(task).not_to be_valid
+      end
+    end
+
     context 'タスクの属性全てに値が入っている場合' do
       it 'タスクを登録できる' do
         task = FactoryBot.build(:task)
