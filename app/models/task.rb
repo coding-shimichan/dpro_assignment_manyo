1236e9fbf44409120dfc9eb51d7cee28fb9635b3
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-30 11:20:59 +0900

    Feat: Add attributes to Task - deadline_on, priority, status

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,14 @@
 class Task < ApplicationRecord
+  # Attributes
+  enum priority: { low: 0, medium: 1, high: 2 }
+  enum status: { not_started: 0, in_progress: 1, completed: 2 }
+
   # Validations
   validates :title, presence: true
   validates :content, presence: true
+  validates :deadline_on, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
 
   # Config for pagination (using gem 'kaminari')
   paginates_per 10
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,10 +2,16 @@ FactoryBot.define do
   factory :task do
     title { '書類作成' }
     content { '企画書を作成する。' }
+    deadline_on { Date.today }
+    priority { 1 }
+    status { 0 }
   end
 
   factory :second_task, class: Task do
     title { 'メール送信' }
     content { '顧客へ営業のメールを送る。' }
+    deadline_on { Date.today }
+    priority { 1 }
+    status { 0 }
   end
 end
