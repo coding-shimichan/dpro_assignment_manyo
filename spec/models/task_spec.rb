10a1e66f60799c077fa316556efd2f8ca7f57e7a
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-31 09:51:15 +0900

    Test: Task scopes

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -44,4 +44,46 @@ RSpec.describe Task, type: :model do
       end
     end
   end
+
+  describe '検索機能' do
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      let!(:first_task) { FactoryBot.create(:task, title: 'aaa_task', status: "not_started") }
+      let!(:second_task) { FactoryBot.create(:task, title: 'bbb_task', status: "in_progress") }
+      let!(:third_task) { FactoryBot.create(:task, title: 'aaa_bbb_task', status: "completed") }
+
+      it "検索ワードを含むタスクが絞り込まれる" do
+        result = Task.fuzzy_search("aaa")
+        expect(result).to include(first_task, third_task)
+        expect(result).not_to include(second_task)
+        expect(result.length).to eq 2
+      end
+    end
+
+    context 'scopeメソッドでステータス検索をした場合' do
+      let!(:first_task) { FactoryBot.create(:task, title: 'aaa_task', status: "not_started") }
+      let!(:second_task) { FactoryBot.create(:task, title: 'bbb_task', status: "in_progress") }
+      let!(:third_task) { FactoryBot.create(:task, title: 'aaa_bbb_task', status: "completed") }
+
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        result = Task.in_status("in_progress")
+        expect(result).to include(second_task)
+        expect(result).not_to include(first_task, third_task)
+        expect(result.length).to eq 1
+      end
+    end
+
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      let!(:first_task) { FactoryBot.create(:task, title: 'aaa_task', status: "not_started") }
+      let!(:second_task) { FactoryBot.create(:task, title: 'bbb_task', status: "in_progress") }
+      let!(:third_task) { FactoryBot.create(:task, title: 'aaa_bbb_task', status: "completed") }
+      let!(:forth_task) { FactoryBot.create(:task, title: 'aaa_bbb_ccc_task', status: "in_progress") }
+
+      it "検索ワードをタイトルに含み、かつステータスに完全一致するタスクが絞り込まれる" do
+        result = Task.in_status("in_progress").fuzzy_search("aaa")
+        expect(result).to include(forth_task)
+        expect(result).not_to include(first_task, second_task, third_task)
+        expect(result.length).to eq 1
+      end
+    end
+  end
 end
