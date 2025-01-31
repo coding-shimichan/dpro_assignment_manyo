ebcb4fc483a2073a6df6ed517017b5ecc0f63375
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-31 09:51:41 +0900

    Feat: Task scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,6 +10,13 @@ class Task < ApplicationRecord
   validates :priority, presence: true
   validates :status, presence: true
 
+  # Queries
+  scope :in_status, -> (status) { where(status: status) if status.present? }
+  scope :fuzzy_search, -> (title) { where("title ILIKE ?", "%#{title}%") if title.present? }
+  scope :sorted_by_deadline, -> { order(deadline_on: :asc) }
+  scope :sorted_by_priority, -> { order(priority: :desc) }
+  scope :sorted_by_creation, -> { order(created_at: :desc) }
+
   # Config for pagination (using gem 'kaminari')
   paginates_per 10
 end
