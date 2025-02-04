e19782cd30ba991df9b42cafd2069ecd221d6ac8
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-02-04 15:14:36 +0900

    Test: Model spec - user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,31 +4,44 @@ RSpec.describe 'ユーザモデル機能', type: :model do
   describe 'バリデーションのテスト' do
     context 'ユーザの名前が空文字の場合' do
       it 'バリデーションに失敗する' do
+        user = FactoryBot.build(:user, name: nil)
+        expect(user).not_to be_valid
       end
     end
 
     context 'ユーザのメールアドレスが空文字の場合' do
       it 'バリデーションに失敗する' do
+        user = FactoryBot.build(:user, email: nil)
+        expect(user).not_to be_valid
       end
     end
 
     context 'ユーザのパスワードが空文字の場合' do
       it 'バリデーションに失敗する' do
+        user = FactoryBot.build(:user, password: nil)
+        expect(user).not_to be_valid
       end
     end
 
     context 'ユーザのメールアドレスがすでに使用されていた場合' do
       it 'バリデーションに失敗する' do
+        user = FactoryBot.create(:user)
+        user_fail = FactoryBot.build(:user)
+        expect(user_fail).not_to be_valid
       end
     end
 
     context 'ユーザのパスワードが6文字未満の場合' do
       it 'バリデーションに失敗する' do
+        user = FactoryBot.build(:user, password: "pass")
+        expect(user).not_to be_valid
       end
     end
 
     context 'ユーザの名前に値があり、メールアドレスが使われていない値で、かつパスワードが6文字以上の場合' do
       it 'バリデーションに成功する' do
+        user = User.new(name: "New User", email: "unused.adress@sample.com", password:"password")
+        expect(user).to be_valid
       end
     end
   end
