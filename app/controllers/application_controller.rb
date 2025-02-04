9b9ab58c76b8151a4a74dadfc857c3156a0a6ae1
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-02-04 13:54:57 +0900

    Feat: Limit available pages for logged-in users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,11 @@ class ApplicationController < ActionController::Base
       redirect_to new_session_path
     end
   end
+
+  def logout_required
+    if (logged_in?)
+      flash[:alert] = t("errors.messages.logout_required")
+      redirect_to tasks_path
+    end
+  end
 end
