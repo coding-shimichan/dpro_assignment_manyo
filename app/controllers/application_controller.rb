985994034293f5ead19f5993d57905a31a21ef2e
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-02-04 13:54:40 +0900

    Feat: Limit available pages for guest users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :login_required
+
+  private
+
+  def login_required
+    if (logged_in? === false)
+      flash[:alert] = t("errors.messages.login_required")
+      redirect_to new_session_path
+    end
+  end
 end
