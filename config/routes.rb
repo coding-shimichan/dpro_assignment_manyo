1ca59c791e85290da080b8e05c14850b7997b40b
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-02-04 17:25:57 +0900

    Feat: Admin user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: redirect("/tasks")
 
   resources :users
+  namespace :admin do
+    resources :users
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
