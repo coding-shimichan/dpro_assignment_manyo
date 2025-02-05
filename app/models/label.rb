dc101906408957854682c8c03db8e254cb12eba0
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-02-05 11:00:34 +0900

    Feat: Label - views, controller and routes

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,2 +1,10 @@
 class Label < ApplicationRecord
+  has_many :task_labels, dependent: :destroy
+  has_many :tasks, through: :task_labels
+
+  validates :name, presence: true, uniqueness: true
+
+  def task_count
+    self.tasks.length
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
   resources :tasks
+  resources :labels
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
